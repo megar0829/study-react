b5d0d640d091781aaa918d5232cb503ebf5470ca
megar0829 <megar0829@gmail.com>
2024-01-15 00:48:06 +0900

    Study React - Soaple

--- soaple/src/index.js
+++ soaple/src/index.js
@@ -8,13 +8,18 @@ import reportWebVitals from './reportWebVitals';
 // import CommentList from './chapter_05/CommentList';
 // import NotificationList from './chapter_06/NotificationList';
 // import Accommodate from './chapter_07/Accommodate';
-import ConfirmButton from './chapter_08/ConfirmButton';
+// import ConfirmButton from './chapter_08/ConfirmButton';
+// import LandingPage from './chapter_09/LandingPage';
+// import AttendanceBook from './chapter_10/AttendanceBook';
+// import SignUp from './chapter_11/SignUp';
+// import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <ConfirmButton />
+    <ProfileCard />
   </React.StrictMode>
 );
 
